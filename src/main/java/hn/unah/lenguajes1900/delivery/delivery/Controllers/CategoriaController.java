652327b3147e9de140bc5cdfbcb9e84a42f1dc6f
CristhianGT64 <crislopez_84@hotmail.es>
2024-04-16 00:01:00 -0600

    Se termino el metodo rest para traer todos las categprias

--- src/main/java/hn/unah/lenguajes1900/delivery/delivery/Controllers/CategoriaController.java
+++ src/main/java/hn/unah/lenguajes1900/delivery/delivery/Controllers/CategoriaController.java
@@ -2,16 +2,27 @@ package hn.unah.lenguajes1900.delivery.delivery.Controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import hn.unah.lenguajes1900.delivery.delivery.Services.impl.CategoriaServiceImpl;
+import hn.unah.lenguajes1900.delivery.delivery.entities.Categoria;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 @RequestMapping("/api")
 public class CategoriaController {
-    @GetMapping("/")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
+
+    @Autowired
+    private CategoriaServiceImpl categoriaServiceImpl;
+
+    @GetMapping("/Categoria/TraerCategorias")
+    public List<Categoria> TraerCatgeorias() {
+        return this.categoriaServiceImpl.TraerCategorias();
     }
     
 }
--- src/main/java/hn/unah/lenguajes1900/delivery/delivery/entities/Categoria.java
+++ src/main/java/hn/unah/lenguajes1900/delivery/delivery/entities/Categoria.java
@@ -2,6 +2,8 @@ package hn.unah.lenguajes1900.delivery.delivery.entities;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -22,6 +24,7 @@ public class Categoria {
 
     private String nombre;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "categoria")
     private List<Producto> producto;
 }
